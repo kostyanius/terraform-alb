462d34540757d95c0c80e2716953547fe6243677
kostyanius <kostyanius@gmail.com>
2020-09-28 21:40:54 +0300

    adding ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+# Looking for Ubuntu 20.04 image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# AWS instance
+resource "aws_instance" "ec2-0" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.medium"
+  subnet_id               =  "${aws_subnet.public.id}"
+  associate_public_ip_address = "true"
+  key_name = "${aws_key_pair.ec2key.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.sg.id}"]
+
+  tags = {
+    Name = "ec2-0"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
