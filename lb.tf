4638c0d6c638b019d1d26db610dc80726e801602
kostyanius <kostyanius@gmail.com>
2020-09-29 02:04:18 +0300

    Added accociation between elastic ip and load balancer

--- lb.tf
+++ lb.tf
@@ -3,11 +3,18 @@ resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "network"
-  subnets            = [aws_subnet.public.id]
+#  subnets            = [aws_subnet.public.id]
   depends_on = [aws_internet_gateway.igw]
   enable_deletion_protection = false
 
   tags = {
     Environment = "test"
+  }
+
+
+ # Mapping for elastic ip and load balancer
+ subnet_mapping {
+    subnet_id     = aws_subnet.public.id
+    allocation_id = aws_eip.lb.id
   }
 }
