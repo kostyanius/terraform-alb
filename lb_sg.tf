2442fad6a82dabdb1f2f37ae20a6b826b8bf5610
kostyanius <kostyanius@gmail.com>
2020-09-29 15:43:40 +0300

    Added commnt for the application LB security group

--- lb_sg.tf
+++ lb_sg.tf
@@ -1,20 +1,21 @@
+#Security group for application load balancer
 resource "aws_security_group" "lb_sg" {
-  name = "allow_http"
+  name   = "allow_http"
   vpc_id = aws_vpc.main.id
 
   ingress {
     from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 }
