fc01c062dd67d011034da722d352d7e80c8c7a64
kostyanius <kostyanius@gmail.com>
2020-09-29 15:57:43 +0300

    Added comment for the ssh rule

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# CIDR block for the ssh rule
 variable "ingress_cidr_blocks" {
   description = "List of IPv4 CIDR whitelist ranges to use for ssh ingress rule"
   default     = ["188.239.50.0/24"]
