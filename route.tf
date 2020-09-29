da4ca2755e9cc27330080290daf657fd49d98c6f
kostyanius <kostyanius@gmail.com>
2020-09-29 15:52:35 +0300

    Added comment for the IGW route table

--- route.tf
+++ route.tf
@@ -1,8 +1,9 @@
+# Route table for the internet gateway
 resource "aws_default_route_table" "default" {
   default_route_table_id = aws_vpc.main.default_route_table_id
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
 
