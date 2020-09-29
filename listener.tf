28d0b89b7f94fe396eb0e3a06f3d52874a1a1693
kostyanius <kostyanius@gmail.com>
2020-09-29 15:48:46 +0300

    Added comment for the lb listener

--- listener.tf
+++ listener.tf
@@ -1,9 +1,10 @@
+# Applicatiion load balancer listener
 resource "aws_lb_listener" "test" {
   load_balancer_arn = aws_lb.test.arn
   port              = "80"
   protocol          = "HTTP"
-#  ssl_policy        = "ELBSecurityPolicy-2016-08"
-#  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  #  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
   default_action {
     type             = "forward"
