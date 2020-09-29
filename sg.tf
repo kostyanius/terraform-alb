5adb5a431f82c9f1ed98b605668f397b53dc4ac7
kostyanius <kostyanius@gmail.com>
2020-09-29 15:54:14 +0300

    Added comment for the ec2 security group

--- sg.tf
+++ sg.tf
@@ -1,26 +1,27 @@
+# Security group for the ec2 instance
 resource "aws_security_group" "sg-ec2" {
-  name = "allow_all"
+  name   = "allow_all"
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = var.ingress_cidr_blocks
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 }
